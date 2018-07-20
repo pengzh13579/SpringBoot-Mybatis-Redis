53ab47e75647e6a50a0d0e0980c7b21abff1d070
pengzh13579 <1794822268@qq.com>
2018-07-20 10:41:48 +0800

    study language

--- src/main/java/cn/pzh/person/application/Application.java
+++ src/main/java/cn/pzh/person/application/Application.java
@@ -1,14 +1,17 @@
 package cn.pzh.person.application;
 
+import cn.pzh.person.application.common.MyLocaleResolver;
 import cn.pzh.person.application.common.Person;
 import javax.servlet.http.HttpServletRequest;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
@@ -22,6 +25,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		return new MyLocaleResolver();
 	}
 
 	@RequestMapping("/")
@@ -50,5 +58,18 @@ public class Application {
 	@ResponseBody
 	public String person(HttpServletRequest request){
 		return person.toString();
+	}
+
+	@RequestMapping("/image")
+	public ModelAndView image(HttpServletRequest request){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("image");
+		mav.addObject("img", "1.png");
+		return mav;
+	}
+
+	@RequestMapping("/webjarsDemo")
+	public String webjarsDemo(HttpServletRequest request){
+		return "webjarsDemo";
 	}
 }
